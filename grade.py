b1429953ddf97adb028bf307e0ec3cfa9eeb565a
phyothirikyaw957510-create <phyothirikyaw957510@gmail.com>
2026-09-14 15:48:33 +0630

    added grade.py

---
+++ grade.py
@@ -0,0 +1,9 @@
+name = input("Enter student name: ")
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+
+average = (mark1 + mark2) / 2
+
+print(f"Student Name: {name}")
+print(f"Average Mark: {average}")
+
