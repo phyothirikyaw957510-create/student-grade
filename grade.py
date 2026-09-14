b2d22086293db309206120d3f30489e0fac59e5d
Phyo Thiri Kyaw <phyothirikyaw957510@gmail.com>
2026-09-14 16:01:49 +0630

    Fix average calculation to divide by 5

--- grade.py
+++ grade.py
@@ -2,7 +2,7 @@ name = input("Enter student name: ")
 mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
 
-average = (mark1 + mark2) / 4
+average = (mark1 + mark2) / 5
 
 print(f"Student Name: {name}")
 print(f"Average Mark: {average}")
